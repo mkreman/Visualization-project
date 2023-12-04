836e2d02a32cf058a65e003f475289125cbce99a
Mayank Nagar <nmayank1998@gmail.com>
2023-12-04 15:48:17 +0530

    folder created

--- Lung_Cancer_ShinyApp/app.R
+++ Lung_Cancer_ShinyApp/app.R
@@ -6,12 +6,12 @@ library(dplyr)
 
 load('./information_gain.RData')
 df <- read.csv('./survey lung cancer.csv')
-df['GENDER'][df['GENDER']=='F'] = '1'
-df['GENDER'][df['GENDER']=='M'] = '2'
-
+df['GENDER'][df['GENDER']=='F'] = 1
+df['GENDER'][df['GENDER']=='M'] = 2
+df1 <- df %>% mutate_all(~ ifelse(. == 1, "NO", ifelse(. == 2, "YES", .)))
 
 # Function to plot bar graphs
-bar.graph <- function(variable, a, b, title){
+bar.graph <- function(variable, a, b){
   data = c(sum(df['LUNG_CANCER']=='YES' & df[variable] == '1') / sum(df['LUNG_CANCER']=='YES'),
            sum(df['LUNG_CANCER']=='NO' & df[variable] == '1') / sum(df['LUNG_CANCER']=='NO'),
            sum(df['LUNG_CANCER']=='YES' & df[variable] == '2') / sum(df['LUNG_CANCER']=='YES'),
@@ -23,10 +23,9 @@ bar.graph <- function(variable, a, b, title){
   
   p <- ggplot(al.freq, aes(x = variable , y = Value, fill = Symptom)) +
     geom_bar(stat = "identity", position = "dodge") +
-    labs(x = "", y = "Probabilities", title = title) +
+    labs(x = "", y = "Probabilities", title = '') +
     geom_text(aes(label=round(Value, 2)), vjust=-0.5, size=4, hjust=0.5,
-              position=position_dodge(width = 0.9)) +
-    theme(plot.title = element_text(hjust = 0.5))
+              position=position_dodge(width = 0.9))
   return(p)
 }
 
@@ -49,7 +48,7 @@ ui <- dashboardPage(
               
               fluidRow(div(
                 style = "display: flex; justify-content: center; align-items: center; height: 36vh;",
-                img(src = "https://github.com/nagar-mayank/Visualization-project/blob/main/Lung_Cancer_ShinyApp/www/IMG_0349-removebg-preview.png?raw=true",
+                img(src = "https://github.com/nagar-mayank/Visualization-project/blob/main/Lung_Cancer_ShinyApp/www/image1.png?raw=true",
                                       height = "250px", width = "350px"))),
               HTML('<p style="font-family: Times New Roman; font-size: 25px; margin-left: 20px;"
                    >Lung cancer continues to pose a significant global health challenge. While extensive research has delved
@@ -61,19 +60,25 @@ ui <- dashboardPage(
                    >I've utilized the lung cancer dataset sourced from <a href='https://www.kaggle.com/datasets/mysarahmadbhat/lung-cancer'
                    target='_blank'>Kaggle</a>, calculating the information gain for each feature.
                    The analysis reveals that `ALLERGY` exhibits the highest information gain, while `SMOKING` demonstrates the 
-                   least. This unexpected outcome is visually represented in the Graph section of the Dashboard, showcasing
-                   information gain values for better insight.</p>"),
+                   least. This unexpected outcome is visually represented in the Information Gain section of the Dashboard, showcasing
+                   IG values for better insight.</p>"),
               ),
-      
+      # Code for 'Graph' Page
       tabItem(tabName = "Graphs",
-              box(title = "Plot 1", status = "primary", solidHeader = TRUE,
-                  fluidRow(column(4, selectInput("barplot_var", "Select a variable:", choices = res$column, selected = res$column[1]))),
-                  fluidRow(column(8, plotOutput('var_barplot', height = "300px", width = "500px"))))
+              fluidRow(column(5, selectInput("graph_var", "Select a variable:", choices = res$column, selected = res$column[1]))),
+              
+              box(title = "Grouped Boxplot of Age given the variable", status = "primary", solidHeader = TRUE,
+                  fluidRow(column(12, plotOutput('stacked_boxplot', width='100%')))),
+              
+              box(title = "Barplot showing distribution of the variable", status = "primary", solidHeader = TRUE,
+                  fluidRow(column(12, plotOutput('barplot', width='100%'))))
       ),
+      
+      # Code for 'Information Gain' Page
       tabItem(tabName = "inf_gain",
-              box(title = "Information Gain Plot", status = "primary", solidHeader = TRUE,
+              box(title = "Information Gain of each variable", status = "primary", solidHeader = TRUE,
                 fluidRow(column(12, plotOutput('information_grain_plot', height = "380px", width = "100%")))),
-              box(title = "Plot 1", status = "primary", solidHeader = TRUE,
+              box(title = "Barplot of Information Gain of a variable", status = "primary", solidHeader = TRUE,
                 fluidRow(column(4, selectInput("variable", "Select a variable:", choices = res$column, selected = res$column[1]))),
                 fluidRow(column(8, plotOutput('prob_barplot', height = "300px", width = "500px"))))
               )
@@ -83,39 +88,35 @@ ui <- dashboardPage(
 
 
 server <- function(input, output) {
-  
-  output$img1 = renderImage({
-    list(src = "https://github.com/nagar-mayank/Visualization-project/blob/main/Lung_Cancer_ShinyApp/www/image1.png?raw=true", width = "600px", height = "300px")},deleteFile=F)
-  
-  output$age_dist <- renderPlot({
-    ggplot(df, aes(x=AGE, color=LUNG_CANCER)) + 
-      geom_histogram(fill='lightblue', aes(y=..density..), bins = 30) + 
-      geom_density(alpha=0.0, fill='#FF0000') + 
-      scale_x_continuous(breaks = seq(min(df$AGE)-1, max(df$AGE), by = 10)) + 
-      labs(x = "Age", y = "Density", title = 'Stacked histogram of Age given the Lung cancer') + 
-      theme(plot.title = element_text(hjust = 0.5))
-  })
-  
   output$information_grain_plot <- renderPlot({
     ggplot(res, aes(x=column, y=information)) + 
       geom_bar(stat="identity", fill='steelblue') + 
       theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-      labs(x = "Features", y = "Information Gain", title = 'Barplot of Information Gain of features') + 
+      labs(x = "Features", y = "Information Gain", title = '') + 
       theme(plot.title = element_text(hjust = 0.5))
   })
   
   output$prob_barplot <- renderPlot({
-    bar.graph(input$variable, paste('Non', input$variable, sep='-'), input$variable, title=paste('Barplot of Probabilities of', input$variable))
+    bar.graph(input$variable, paste('Non', input$variable, sep='-'), input$variable)
   })
   
-  output$var_barplot <- renderPlot({
-    ggplot(res, aes(y=input$barplot_var)) + 
-      geom_bar(stat="identity", fill='steelblue') + 
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-      labs(x = "Feature", y = "Frequency", title = 'Barplot of frequency of variable') + 
+  output$stacked_boxplot <- renderPlot({
+    ggplot(df1, aes(y = AGE, color = !!sym(input$graph_var))) +
+      geom_boxplot() + 
+      labs(y = "Age",
+           title = '') + 
       theme(plot.title = element_text(hjust = 0.5))
   })
+  
+  output$barplot <- renderPlot({
+    ggplot(df1, aes(x = get(input$graph_var))) +
+      geom_bar(fill = "#0073C2FF", color = "black", alpha = 0.7) +
+      labs(x = input$graph_var, y = "Count", title = '') +
+      theme_minimal()
+  })
+  
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- Lung_Cancer_ShinyApp/test.R
+++ Lung_Cancer_ShinyApp/test.R
@@ -1,41 +1,21 @@
 library(ggplot2)
-
-df = read.csv('C:/Users/MkReman/Gdrive/Goodnote/CMI Data Science/Semester - 1/Visualization/Visualization-project/Lung_Cancer_ShinyApp/survey lung cancer.csv')
-
-table(df[['SMOKING']])
-
-ggplot(df, aes(y='SMOKING')) + 
-  geom_bar(stat="identity", fill='steelblue') + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-  labs(x = "Feature", y = "Frequency", title = 'Barplot of frequency of variable') + 
-  theme(plot.title = element_text(hjust = 0.5))
-
-
-col = 'SMOKING'
-length(df[col][df['LUNG_CANCER'] == 'YES'])
-length(df[col][df['LUNG_CANCER'] == 'NO'])
-
-library(ggplot2) 
 library(dplyr) 
 
 
-###############################
+df = read.csv('C:/Users/MkReman/Gdrive/Goodnote/CMI Data Science/Semester - 1/Visualization/Visualization-project/Lung_Cancer_ShinyApp/survey lung cancer.csv')
 
+col = 'SMOKING'
 
 count.data <- data.frame( 
-  pilot_class = c(col, paste('Non-', col)), 
-  n = c(length(df[col][df['LUNG_CANCER'] == 'YES']), length(df[col][df['LUNG_CANCER'] == 'NO'])), 
-  proportion = c(length(df[col][df['LUNG_CANCER'] == 'YES'])/(length(df[col][df['LUNG_CANCER'] == 'YES'])+length(df[col][df['LUNG_CANCER'] == 'NO'])),
-                 length(df[col][df['LUNG_CANCER'] == 'NO'])/(length(df[col][df['LUNG_CANCER'] == 'YES'])+length(df[col][df['LUNG_CANCER'] == 'NO']))) 
+  pilot_class = c(col, paste('Non-', col, sep='')), 
+  n = c(length(df1[col][df1[col] == 'YES']), length(df1[col][df1[col] == 'NO'])),
+  proportion = c(length(df1[col][df1[col] == 'YES'])/(nrow(df1)),
+                 length(df1[col][df1[col] == 'NO'])/(nrow(df1))) 
 ) 
-
-count.data 
 
 count.data <- count.data %>% 
   arrange(desc(pilot_class)) %>% 
   mutate(lab.ypos = cumsum(proportion) - 0.6*proportion) 
-
-count.data 
 
 ggplot(count.data, aes(x = "", y = proportion, fill = pilot_class)) + 
   geom_bar(width = 1, stat = "identity", color = "black") + 
@@ -62,9 +42,11 @@ ggplot(data, aes(x = "", y = value, fill = category)) +
   ggtitle("Pie Chart Example")
 
 #############################
-ggplot(df, aes(y = AGE, color=col)) +
+df1 <- df %>% mutate_all(~ ifelse(. == 1, "NO", ifelse(. == 2, "YES", .)))
+
+col = 'ALCOHOL.CONSUMING'
+ggplot(df1, aes(y = AGE, color=ALCOHOL.CONSUMING)) +
   geom_boxplot() + 
-  ggtitle('Boxplot Between GENDER and AGE') + 
   labs(x = "Gender of Individual", y = "Age",
-       title = 'Grouped Boxplot between Gender and Age given the Lung cancer') + 
+       title = 'Grouped Boxplot of Age') + 
   theme(plot.title = element_text(hjust = 0.5))
